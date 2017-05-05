0af79ee0a5f6d5f25feebddf897a619d8de26dd9
Achmad Kurnianto <achmad.kurnianto@gmail.com>
2017-05-05 22:29:32 +0700

    update countdown calculation

--- src/format-date.js
+++ src/format-date.js
@@ -1,7 +1,36 @@
 import moment from 'moment';
 
-export default function formatDate(fromDate, toDate, mask) {
-  const delta = new Date(toDate) - new Date(fromDate);
+export default function formatDate(toDate, targetFormatMask, sourceFormatMask) {
+  let countdownMillisecond = getDelta(toDate, sourceFormatMask)
+  let duration = moment.duration(countdownMillisecond)
+  let finalTargetFormatMask = editTargetFormatMask(duration, targetFormatMask)
+  let countdownString = moment(duration._data).format(targetFormatMask)
 
-  return moment(delta).format(mask);
+  return [countdownMillisecond, countdownString];
+}
+
+export function getDelta(toDate, sourceFormatMask) {
+  if (!moment.isMoment(toDate)) {
+    let convert = moment.isDate(toDate)
+      ? moment(toDate)
+      : moment(toDate, sourceFormatMask)
+    return convert.diff(moment())
+  }
+  else return toDate.diff(moment())
+}
+
+function editTargetFormatMask (duration, targetFormatMask) {
+  let changeDate = []
+  if (duration.months() == 0) changeDate.push('M')
+  if (duration.days() == 0) changeDate.push('D')
+  return changeDate.reduce((acc, char) => {
+    let charCount = acc.split('').filter(tfm => tfm == char).length
+    let charBefore = generateString(char, charCount)
+    let charAfter = `[${generateString('0', charCount)}]`
+    return acc.replace(charBefore, charAfter)
+  }, targetFormatMask)
+}
+
+function generateString (character, count) {
+  return Array(count).fill(character).join('')
 }
