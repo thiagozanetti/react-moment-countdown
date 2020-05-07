a1eb07c47651424cacdcfd9ce3b7c22e754cb226
Thiago Zanetti <zanetti@devgrid.co.uk>
2020-05-06 23:04:27 -0300

    fixing the display string formatting to get from the most granular value of the duration

--- src/format-date.js
+++ src/format-date.js
@@ -1,16 +1,17 @@
 import moment from 'moment';
 
-export default function formatDate (toDate, targetFormatMask, sourceFormatMask) {
+export default function formatDate(toDate, targetFormatMask, sourceFormatMask) {
   // to prevent from returning negative duration
   const countdownMillisecond = Math.max(0, getDelta(toDate, sourceFormatMask));
   const duration = moment.duration(countdownMillisecond);
-  const finalTargetFormatMask = editTargetFormatMask(duration, targetFormatMask);
-  const countdownString = moment(duration._data).format(finalTargetFormatMask);
+
+  //to correctly display the countdown from the most granular unit
+  const countdownString = moment.utc(duration.as('milliseconds')).format(targetFormatMask);
 
   return [countdownMillisecond, countdownString];
 }
 
-export function getDelta (toDate, sourceFormatMask) {
+export function getDelta(toDate, sourceFormatMask) {
   if (!moment.isMoment(toDate)) {
     const convert = moment.isDate(toDate)
       ? moment(toDate)
@@ -20,22 +21,4 @@ export function getDelta (toDate, sourceFormatMask) {
   }
 
   return toDate.diff(moment());
-}
-
-function editTargetFormatMask (duration, targetFormatMask) {
-  const changeDate = [];
-
-  if (duration.months() === 0) changeDate.push('M');
-  if (duration.days() === 0) changeDate.push('D');
-
-  return changeDate.reduce((acc, char) => {
-    const charCount = acc.split('').filter(tfm => tfm === char).length;
-    const charBefore = generateString(char, charCount);
-    const charAfter = `[${generateString('0', charCount)}]`;
-    return acc.replace(charBefore, charAfter);
-  }, targetFormatMask);
-}
-
-function generateString (character, count) {
-  return Array(count).fill(character).join('');
 }
